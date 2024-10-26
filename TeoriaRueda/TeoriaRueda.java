36f83d2451d162eec6b1d3637be96767d6d98ec5
jiglf <juan.iglesiasfuentes@gmail.com>
2024-10-26 19:15:00 +0200

    cambios en teoria rueda

--- TeoriaRueda/TeoriaRueda.java
+++ TeoriaRueda/TeoriaRueda.java
@@ -5,6 +5,7 @@ public class TeoriaRueda {
         System.out.println(" ***** TEORIA DE LA RUEDA *****");
         teoriaRuedaByte();
         teoriaRuedaInteger();
+        teoriaRuedaLong();
 
         
 
@@ -34,11 +35,11 @@ public class TeoriaRueda {
         System.out.println(" * Teoria de la rueda Byte");
         System.out.println("Vamos a ver la teoria de la rueda, cuando una variable llega a su valor maximo vuelve a empezar");
         
-        byte b1 = 127, b2 = 1, b3;
+        byte b1 = Byte.MAX_VALUE, b2 = 1, b3;
 
-        System.out.println("Vamos a realizar la suma de " + b1 + " y de " + b2 + ", vamos a sumarlo en una variable b3 casteada a byte");
+        System.out.println("Vamos a realizar la suma de " + b1 + " y de " + b2);
 
-        b3 = (byte)(b1 + b2);
+        b3 = (byte)(b1 + b2);//es necesario casteo para que no se pase a int
         
         System.out.println("El restutado de b3 es " + b3 + ", esta es la teoria de la rueda vuelve a empezar en el valor minimo");
 
@@ -51,14 +52,31 @@ public class TeoriaRueda {
         System.out.println(" * Teoria de la rueda Integer");
         System.out.println("Vamos a ver la teoria de la rueda, cuando una variable llega a su valor maximo vuelve a empezar");
 
-        int i1 = 2147483647, i2 = 1, i3;
+        int i1 = Integer.MAX_VALUE, i2 = 1, i3;
 
-        System.out.println("Vamos a realizar la suma de " + i1 + " y de " + i2 + ", vamos a sumarlo en una variable i3 casteada a int");
+        System.out.println("Vamos a realizar la suma de " + i1 + " y de " + i2);
 
         i3 = i1 + i2;
 
-        System.out.println("Comprobamos que vuelva a su valor minimo " + i3);
+        System.out.println("Comprobamos que vuelva a su valor minimo " + i3 + ",  esta es la teoria de la rueda vuelve a empezar en el valor minimo");
+        System.out.println();
 
+
+    }
+
+    public static void teoriaRuedaLong() {
+
+        System.out.println(" * Teoria de la rueda Long");
+        System.out.println("Vamos a ver la teoria de la rueda, cuando una variable llega a su valor maximo vuelve a empezar");
+
+        long l1 = Long.MAX_VALUE, l2 = 1, l3;
+
+        System.out.println("Vamos a realizar la suma de " + l1 + " y de " + l2);
+
+        l3 = l1 + l2;
+
+        System.out.println("Comprobamos que vuelva a su valor minimo " + l3 + ",  esta es la teoria de la rueda vuelve a empezar en el valor minimo");
+        System.out.println();
 
 
     }
